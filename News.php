d5a92c30c907b8a5261b47de9df4bf1d767765d0
Dobrodeev <dobrod@meta.ua>
2018-09-15 22:54:32 +0300

    исправили метод delete

--- News.php
+++ News.php
@@ -25,12 +25,12 @@ class News extends Controller
 
     public function delete($name_ofnew)
     {
-//        $sth = $this->dbh->query('DELETE FROM News WHERE name="' . $name_ofnew . '"');
+        $sth = $this->dbh->query('DELETE FROM News WHERE name="' . $name_ofnew . '"');
 //        echo 'DELETE FROM News WHERE name_ofnew="' . $name_ofnew . '"';
-
-//        header('Location: /news.html');
-        $this->delete_from_db("News", "name", $name_ofnew);
+        header('Location: /news.html');
+        //$this->delete_from_db("News", "name", $name_ofnew);
     }
+    
     public function show()
     {
         $sth = $this->dbh->query("SELECT * FROM News;");
@@ -47,8 +47,6 @@ class News extends Controller
 //            echo var_dump($element)."<hr>";
             echo "<h3>$element[1]</h3> <p>$element[2]</p><img src='$element[3]'><hr/> ";
         }
-
-
         //header('Location: /news.html');
     }
 }
