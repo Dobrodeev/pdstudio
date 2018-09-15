fb2d36436fa1afe0ec897dbd34ff6526b5d7b931
Dobrodeev <dobrod@meta.ua>
2018-09-15 22:43:32 +0300

    убрали лишний метод

--- Controller.php
+++ Controller.php
@@ -9,16 +9,9 @@ class Controller
 {
     protected $dbh;
 
-
     public function __construct()
     {
         $this->dbh = new PDO('mysql:host=localhost;dbname=' . DB_CONNECTION_DB, DB_CONNECTION_USER, DB_CONNECTION_PASS);
 
-    }
-    protected function delete_from_db($table, $name_param, $value_param)
-    {
-//        $query="DELETE FROM $table WHERE $name_param='$value_param'";
-        $query = 'DELETE FROM $table ';
-        $sth = $this->dbh->query($query);
     }
 }
